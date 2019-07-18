97dc80578c5d8163faca4f0a0b342e1a4da5e233
bw2 <weisburd@broadinstitute.org>
2019-07-18 01:29:19 -0400

    added vep, gnomAD AC & AF fields

--- laura_denovos_gmkf/generate_de_novo_table.py
+++ laura_denovos_gmkf/generate_de_novo_table.py
@@ -10,7 +10,7 @@ print(subprocess.check_output(
     "/opt/conda/default/bin/python3 -m pip install git+https://github.com/bw2/hail-utils.git", 
     shell=True, encoding="UTF-8"))
 
-from hail_utils.vep import compute_derived_vep_fields, get_expr_for_vep_sorted_transcript_consequences_array
+from hail_utils.vep import compute_derived_vep_fields, get_expr_for_vep_sorted_transcript_consequences_array, get_expr_for_vep_consequence_terms
 from hail_utils.info_fields import recompute_AC_AN_AF, annotate_in_LCR, annotate_in_segdup
 from hail_utils.filters import filter_out_variants_where_all_samples_are_hom_ref, filter_out_LCRs, filter_out_segdups, filter_to_autosomes
 from hail_utils.variants import get_expr_for_variant_type
@@ -32,6 +32,7 @@ PCR_PLUS_VCF_PATH = os.path.join(BASE_DIR, "GMKF_Gabriel_Luquetti_Craniofacial_W
 RGP_VCF_PATH = "gs://seqr-datasets/GRCh38/RDG_WGS_Broad_Internal/v7/sharded_vcf/RDG_WGS_Broad_Internal.filtered.*.vcf.gz"
 
 force = args.force
+algo_label = "mendel" if args.mendelian else "samocha"
 
 for data_label in args.data_label:
     print(f"Processing {data_label}. Force: {force}")
@@ -99,8 +100,8 @@ for data_label in args.data_label:
     print(len(pedigree.complete_trios())*3)
     print(len(pedigree.complete_trios())*3/len(vcf_samples))
 
+    file_path = os.path.join(BASE_DIR, f"{data_label}.{algo_label}_denovos.ht")
     if args.mendelian:
-        file_path = os.path.join(BASE_DIR, f"{data_label}.mendel_denovos.ht")
         if not file_exists(file_path):
             print(f"{data_label}: Generating {file_path}")
             denovos = compute_mendel_denovos(mt, pedigree)
@@ -110,7 +111,6 @@ for data_label in args.data_label:
             denovos = hl.read_table(file_path)
 
     else:
-        file_path = os.path.join(BASE_DIR, f"{data_label}.samocha_denovos.ht")
         if not file_exists(file_path):
             print(f"{data_label}: Generating {file_path}")
             denovos = compute_samocha_denovos(mt, pedigree)
@@ -119,54 +119,65 @@ for data_label in args.data_label:
             print(f"Reading table {file_path}")
             denovos = hl.read_table(file_path)
 
-    #file_path = os.path.join(BASE_DIR, f"{data_label}.mendel_denovos.vep.ht")
-    #if not file_exists(file_path):
-    #    print(f"{data_label}: Generating {file_path}")
-    #    denovos = denovos.key_by('locus', 'alleles')
-    #    denovos = hl.vep(denovos, "file:///vep_data/vep-gcloud.json", name="vep", block_size=100)
-    #
-    #    denovos = denovos.checkpoint(file_path, overwrite=True, _read_if_exists=not force)
-    #else:
-    #    print(f"Reading table {file_path}")
-    #    denovos = hl.read_table(file_path)
+    file_path = os.path.join(BASE_DIR, f"{data_label}.{algo_label}_denovos.vep.ht")
+    if not file_exists(file_path):
+        print(f"{data_label}: Generating {file_path}")
+        denovos = denovos.key_by('locus', 'alleles')
+        denovos = hl.vep(denovos, "file:///vep_data/vep-gcloud.json", name="vep", block_size=100)
+
+        denovos = denovos.checkpoint(file_path, overwrite=True, _read_if_exists=not force)
+    else:
+        print(f"Reading table {file_path}")
+        denovos = hl.read_table(file_path)
 
 
-    #print(f"{data_label}: annotate and export {file_path}")
-    #denovos = denovos.annotate(sorted_transcript_consequences = get_expr_for_vep_sorted_transcript_consequences_array(denovos.vep))
-    #denovos = denovos.annotate(transcript_consequence_categories = denovos.sorted_transcript_consequences.map(lambda c: c.category))
-    #denovos = denovos.annotate(transcript_consequence_category = hl.cond(hl.len(denovos.transcript_consequence_categories) > 0, denovos.transcript_consequence_categories[0], "other"))
-        
+    print(f"{data_label}: annotate and export {file_path}")
+    denovos = denovos.annotate(sorted_transcript_consequences = get_expr_for_vep_sorted_transcript_consequences_array(denovos.vep))
+    denovos = denovos.annotate(transcript_consequence_terms = get_expr_for_vep_consequence_terms(denovos.sortedTranscriptConsequences))
+    denovos = denovos.annotate(transcript_consequence_categories = denovos.sorted_transcript_consequences.map(lambda c: c.category))
+    denovos = denovos.annotate(transcript_consequence = hl.cond(hl.len(denovos.transcript_consequence_terms) > 0, denovos.transcript_consequence_terms[0], "other"))
+    denovos = denovos.annotate(transcript_consequence_category = hl.cond(hl.len(denovos.transcript_consequence_categories) > 0, denovos.transcript_consequence_categories[0], "other"))
+
     joined_mt_rows = mt.rows()[(denovos.locus, denovos.alleles)]
 
-    if args.mendelian:
-        file_path = os.path.join(BASE_DIR, f"{data_label}.mendel_de_novos_table.tsv")
-    else:
-        file_path = os.path.join(BASE_DIR, f"{data_label}.samocha_de_novos_table.tsv")
+    gnomad_genomes = hl.read_table("gs://seqr-reference-data/GRCh38/gnomad/gnomad.genomes.r2.1.1.sites.liftover_grch38.ht")
+    gnomad_exomes = hl.read_table("gs://seqr-reference-data/GRCh38/gnomad/gnomad.exomes.r2.1.1.sites.liftover_grch38.ht")
 
+    joined_gnomad_genomes_ht = gnomad_genomes[(denovos.locus, denovos.alleles)]
+    joined_gnomad_exomes_ht = gnomad_exomes[(denovos.locus, denovos.alleles)]
+
+    file_path = os.path.join(BASE_DIR, f"{data_label}.{algo_label}_de_novos_table.tsv")
     denovos = denovos.annotate(
         dataset=data_label,
         variant_type=get_expr_for_variant_type(denovos),
         in_LCR=joined_mt_rows.info.in_LCR,
         in_segdup=joined_mt_rows.info.in_segdup,
+        filters=hl.cond(hl.len(joined_mt_rows.filters) > 0, hl.delimit(joined_mt_rows.filters, ','), 'PASS'),
         AC=joined_mt_rows.info.AC,
         AF=joined_mt_rows.info.AF, 
-        QD=joined_mt_rows.info.QD)
+        QD=joined_mt_rows.info.QD,
+        gnomAD_genomes_AF = joined_gnomad_genomes_ht.freq[0].AF,
+        gnomAD_genomes_AC = joined_gnomad_genomes_ht.freq[0].AC,
+        gnomAD_exomes_AF = joined_gnomad_exomes_ht.freq[0].AF,
+        gnomAD_exomes_AC = joined_gnomad_exomes_ht.freq[0].AC,
+    )
 
     denovos = denovos.key_by()
     if args.mendelian:
         denovos = denovos.select(
             'dataset',
             's', 'mendel_code',
-            'variant_type', 'AC', 'AF', 'QD', 'in_LCR', 'in_segdup')
+            'transcript_consequence', 'transcript_consequence_category',
+            'variant_type', 'filters', 'AC', 'AF', 'QD', 'in_LCR', 'in_segdup')
     else:
         denovos = denovos.select(
             'dataset',
             'confidence',
             'proband', 'father', 'mother', 'proband_AB', 'proband_DP', 'proband_GQ', 'mother_AB', 'mother_DP', 'mother_GQ', 'father_AB', 'father_DP', 'father_GQ',
-            #'transcript_consequence_category',
-            'variant_type', 'AC', 'AF', 'QD', 'in_LCR', 'in_segdup')
+            'transcript_consequence', 'transcript_consequence_category',
+            'variant_type', 'filters', 'AC', 'AF', 'QD', 'in_LCR', 'in_segdup')
 
 
     denovos.export(file_path)
 
-    print(f"Done with {data_label}!")
+    print(f"Done with {data_label} {algo_label}!")
